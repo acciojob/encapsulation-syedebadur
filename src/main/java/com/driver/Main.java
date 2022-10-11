3613c649450a2ebcdd117122dce07fd558245071
syedebadur <syed9702@gmail.com>
2022-10-11 15:29:52 +0530

    Solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Ebadur");
+        System.out.println(obj.getName());
+    }
 }
